7defd136f547c80c57473fa9f1226d190d0f83e4
ikjo <chi4321@naver.com>
2022-12-07 19:35:40 +0900

    build: 초기 설정

---
+++ src/test/java/com/ikjo39/commerce/CommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikjo39.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
